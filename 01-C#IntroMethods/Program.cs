9c08226fd8b322ea894661090495953fc1f3ca90
elcan101 <eljankha-apa103@code.edu.az>
2026-03-13 11:35:11 -0700

    c#IntroMethods

---
+++ 01-C#IntroMethods/Program.cs
@@ -0,0 +1,96 @@
+﻿//task4
+//string s = Console.ReadLine();
+//char c = 'a';
+//Console.WriteLine(findCount(s,c));
+//int findCount(string s,char c)
+//{
+//    int count = 0;
+//    for (int i = 1; i < s.Length-1; i++)
+//    {
+//        if (s[i]==c) 
+//        {
+//            count++;
+//        }
+//    }
+//    return count;
+//}
+
+
+
+
+//task3
+//int[] arr = new int[] { 14, 20, 35, 40, 57, 60, 100};
+//int cem = sum(arr);
+//Console.WriteLine(cem);
+//static int sum(int[] arr) 
+//{
+//    int result = 0;
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] % 4 == 0 && arr[i] % 5 == 0) result += arr[i];
+//    }
+//    return result;
+
+//}
+
+
+
+
+//task1
+//    Console.WriteLine(Topla(4, 5));
+//    Console.WriteLine(Cix(5, 6));
+//    Console.WriteLine(Vur(4, 5));
+//    Console.WriteLine(Bol(14, 7));
+
+//static double Topla(double a, double b)
+//{
+//    return a + b;
+//}
+
+
+//static double Cix(double a, double b)
+//{
+//    return a - b;
+//}
+
+
+//static double Vur(double a, double b)
+//{
+//    return a * b;
+//}
+
+//static double Bol(double a, double b)
+//{
+//    if (b != 0)
+//    {
+//        return a / b;
+//    }
+//    else return 0;
+
+//}
+
+
+
+
+//task2
+//int[] arr = new int[] { 14, 20, 35, 40, 57, 60, 100 };
+//cutdurmu(arr);
+//tekdirmi(arr);
+//void cutdurmu(int[] massiv) 
+//{
+//    for (int i = 0; i < massiv.Length; i++)
+//    {
+//        if (massiv[i] % 2 == 0) Console.WriteLine(massiv[i]);
+
+//    }
+
+//}
+//void tekdirmi(int[] arr)
+//{
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] % 2 == 1) Console.WriteLine(arr[i]);
+
+//    }
+
+//}
