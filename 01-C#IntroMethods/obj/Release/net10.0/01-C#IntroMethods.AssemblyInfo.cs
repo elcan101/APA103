9b57871c3d053b221fed2c829713f4c3ad139c1d
elcan101 <eljankha-apa103@code.edu.az>
2026-03-14 10:41:23 -0700

    02-HeapStackRefOutArrayResize

--- 01-C#IntroMethods/obj/Release/net10.0/01-C#IntroMethods.AssemblyInfo.cs
+++ 01-C#IntroMethods/obj/Release/net10.0/01-C#IntroMethods.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("01-C#IntroMethods")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c08226fd8b322ea894661090495953fc1f3ca90")]
 [assembly: System.Reflection.AssemblyProductAttribute("01-C#IntroMethods")]
---
+++ 02-HeapStackRefOutArrayResize/02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Taskın tələbi budur ki, ArrayResize metodu 2 parametr qəbul edəcək. 1-ci parametr hazır numbers array olacaq
+//2-ci parametr ədədlər çoxluğu göndərib params ilə array kimi qəbul edəcəksiniz və method bu params ilə qəbul 
+//etdiyiniz ədədləri numbers arrayinə əlavə edəcək.
+
+using System.ComponentModel;
+
+int[] numbers = new int[5];
+int[] numbers2 = { 2, 5, 8, 11, 14 };
+ArrayResize(ref numbers, numbers2);
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+
+static void ArrayResize(ref  int[] numbers, params int[] numbers2)
+{
+    for (int i = 0; i < numbers2.Length; i++)
+    {
+        numbers[i] = numbers2[i];
+    }
+}
