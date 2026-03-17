2f8719910c78624ed9d8e13411181ffd78fb6b61
elcan101 <eljankha-apa103@code.edu.az>
2026-03-17 14:15:11 -0700

    encupsalation

---
+++ 04-AccessModifiresEncupsulationNamespace/Models/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_AccessModifiresEncupsulationNamespace.Models
+{
+    internal class Car
+    {
+        public string _model;
+        private int _horsepower;
+
+        public int HorsePower 
+        { 
+            get
+            {
+                return _horsepower;
+            }
+            set
+            {
+                if (value<100)
+                {
+                    Console.WriteLine("enter valid power");
+                }
+                _horsepower = value;
+            }
+                
+          }
+    }
+}
---
+++ 04-AccessModifiresEncupsulationNamespace/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+using _04_AccessModifiresEncupsulationNamespace.Models;
+
+Car car = new Car();
+car.HorsePower = 50;
+Console.WriteLine(car.HorsePower);
