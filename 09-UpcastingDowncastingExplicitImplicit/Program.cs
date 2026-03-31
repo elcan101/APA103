0fe29a9950ea193ab8f526656f00fa8372f07f63
elcan101 <eljankha-apa103@code.edu.az>
2026-03-31 14:37:19 -0700

    costing

---
+++ 09-UpcastingDowncastingExplicitImplicit/Program.cs
@@ -0,0 +1,11 @@
+﻿using _09_UpcastingDowncastingExplicitImplicit.Models;
+
+Dollar dollar = new(200);
+Manat manat = new(170);
+
+Dollar dollar1 = manat;
+Console.WriteLine(dollar1.USD);
+
+Manat manat1 = dollar;
+Console.WriteLine(manat.AZN);
+
