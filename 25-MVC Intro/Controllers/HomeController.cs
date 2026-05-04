ef9ca8b48ec8381017e62898c3a7f129d307f3d8
elcan101 <eljankha-apa103@code.edu.az>
2026-05-04 12:31:01 -0700

    25-MVC Intro

---
+++ 25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        public ViewResult Detail(int? id)
+        {
+        //    if (id is null || id < 1 )
+        //    {
+        //        throw new Exception("Id sehvdir");
+        //    }
+            return View();
+        }
+
+        public ViewResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ 25-MVC Intro/Program.cs
@@ -0,0 +1,15 @@
+namespace _25_MVC_Intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
